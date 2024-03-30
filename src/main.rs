e22855c6bb41af8a038b5dd534039454ceeb181d
Oscar Pinon <oscarpinon@Oscars-MacBook-Air.local>
2024-03-29 19:49:46 -0600

    added stuff

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPool;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPool;
-use std::net::TcpListener;
-use secrecy::ExposeSecret;
 
 /// Compose multiple layers into a `tracing`'s subscriber.
 ///
@@ -16,18 +16,19 @@ use secrecy::ExposeSecret;
 /// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
 /// later on.
 
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("failed to connect to db");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,6 @@
-use actix_web::{HttpResponse, web};
-use sqlx::{PgPool};
-use chrono::{Utc};
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -16,25 +16,20 @@ fields(
     subscriber_name= %form.name
     )
 )]
-pub async fn subscribe(
-    form: web::Form<FormData>, pool: web::Data<PgPool>,
-) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await
-    {
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish() }
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
 }
 
-
 #[tracing::instrument(
-name = "Saving new subscriber details in the database",
-skip(form, pool)
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
 )]
-pub async fn insert_subscriber(
-    pool: &PgPool,
-    form: &FormData,
-) -> Result<(), sqlx::Error> {
-    sqlx::query!( r#"
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
     "#,
@@ -42,7 +37,8 @@ pub async fn insert_subscriber(
         form.email,
         form.name,
         Utc::now()
-    ).execute(pool)
+    )
+    .execute(pool)
     .await
     .map_err(|e| {
         tracing::error!("Failed to execute query: {:?}", e);
