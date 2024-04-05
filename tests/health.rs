0d5077370159dd23ce87490026587f677c358d98
opinon1 <oscar.pinon3@gmail.com>
2024-04-05 12:30:04 -0600

    working email

--- tests/health.rs
+++ tests/health.rs
@@ -43,7 +43,11 @@ async fn spawn_app() -> TestApp {
         .email_client
         .sender()
         .expect("Invalid sender email");
-    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
 
     let connection_pool = configure_database(&configuration.database).await;
     let server =
